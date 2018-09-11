a30eff5a7dc0d7ea54bf41ea2248441cd10dedd0
Eva Royo <e.r.educacion@gmail.com>
2018-09-11 19:11:20 +0200

    Cambios sin middleware

--- server.js
+++ server.js
@@ -3,4 +3,26 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hola Mundo')
 })
-app.listen(3000)
+
+// para ejecutar una función cuando haya sido arrancado el servidor:
+app.listen(3000, () => {
+  console.log('Servidor web arrancado en el puerto 3000')
+})
+
+app.get('/contactar-plano', (req, res) => {
+  res.send('Pagina para contactar');
+});
+
+// ahora devuelve un json
+app.get('/contactar-json-normal', (req, res) => {
+  res.send({
+    nombre: 'pepito',
+    email: 'pepito@gmail.com'
+  })
+})
+
+// ahora devuelve un json de un archivo
+const contacto = require ('./contacto.json')
+app.get('/contactar', (req, res) => {
+  res.send(contacto);
+});
